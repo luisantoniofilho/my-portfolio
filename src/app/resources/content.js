dfc5745b1296e848d3bae850d99f6c6162cd943f
luisantoniofilho <luissantosfilho2014@gmail.com>
2025-06-01 16:59:28 -0300

    feat: update the work label to "Experiência"

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -210,6 +210,15 @@ const about = {
   },
 };
 
+const work = {
+  path: "/work",
+  label: "Experiência",
+  title: `Projects – ${person.name}`,
+  description: `Design and dev projects by ${person.name}`,
+  // Create new project pages by adding a new .mdx file to app/blog/posts
+  // All projects will be listed on the /home and /work routes
+};
+
 const blog = {
   path: "/blog",
   label: "Blog",
@@ -217,15 +226,6 @@ const blog = {
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
-};
-
-const work = {
-  path: "/work",
-  label: "Work",
-  title: `Projects – ${person.name}`,
-  description: `Design and dev projects by ${person.name}`,
-  // Create new project pages by adding a new .mdx file to app/blog/posts
-  // All projects will be listed on the /home and /work routes
 };
 
 const gallery = {
